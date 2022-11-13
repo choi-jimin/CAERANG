ca0f838d3e34da1af1b53507686127af43b933c6
choi-jimin <109493189+choi-jimin@users.noreply.github.com>
2022-11-14 02:09:31 +0900

    api css update

---
+++ SoftwareC/Zoo/app.js
@@ -0,0 +1,63 @@
+/* 영화 검색 날짜 */
+let dateObj = new Date();
+let movieSearch;
+if (dateObj.getDate() < 10) {
+    movieSearch = String(dateObj.getFullYear()) + String(dateObj.getMonth() + 1) + "0" + String(dateObj.getDate()) - 1;
+} else {
+    movieSearch = String(dateObj.getFullYear()) + String(dateObj.getMonth() + 1) + String(dateObj.getDate() - 1);
+}
+
+
+/* api 선언부 ---------------------------------------------------------- */
+
+/* 영화진흥위원회 API */
+
+// url : http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json
+
+let api_key_movie = "key=22f10dd3863febae7e67b8df14051d8b"; // API 키
+let targetDt = "&targetDt=" + movieSearch;
+
+const movie_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?" + api_key_movie + targetDt;
+const movie_detail_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?" + api_key_movie + "&movieCd=";
+
+/** 영화진흥위원회 API fetch 함수 */
+function callFetchMovie() {
+    fetch(movie_url)
+        .then(response => response.json())
+        .then(function (json) {
+            console.log(`${movieSearch} 일자의 데이터입니다.`);
+            let movieJson = json.boxOfficeResult.dailyBoxOfficeList;
+            console.log(movieJson)
+            for (let i = 0; i < movieJson.length; i++) {
+                let plusSection = document.getElementById('section_main_div');
+
+                let new_pTag = document.createElement('p');
+                new_pTag.setAttribute('id', 'new_pTag');
+
+                let new_pTag_movieRank = document.createElement('p');
+                let new_pTag_movieImg = document.createElement('img');
+                let new_pTag_movieCd = document.createElement('p');
+                let new_pTag_movieNm = document.createElement('p');
+                let new_pTag_movieOpenDt = document.createElement('p');
+
+                new_pTag_movieRank.append(`${i + 1}`);
+                new_pTag_movieImg.append(`이미지`);
+                new_pTag_movieCd.append(`${movieJson[i].movieCd}`);
+                new_pTag_movieNm.append(`${movieJson[i].movieNm}`);
+                new_pTag_movieOpenDt.append(`${movieJson[i].openDt.substr(0, 4)}`);
+
+                new_pTag.append(new_pTag_movieRank);
+                new_pTag.append(new_pTag_movieImg);
+                new_pTag.append(new_pTag_movieNm);
+                new_pTag.append(new_pTag_movieCd);
+                new_pTag.append(new_pTag_movieOpenDt);
+
+                new_pTag_movieRank.setAttribute('id', 'movie_rank');
+                new_pTag_movieCd.setAttribute('id', 'movie_code');
+                new_pTag_movieNm.setAttribute('id', 'movie_title');
+
+                plusSection.appendChild(new_pTag);
+            }
+
+        });
+}
--- SoftwareC/movie/movie.js
+++ SoftwareC/movie/movie.js
@@ -1,3 +1,13 @@
+function service() {
+    let signIn = "${signIn}";
+    if (signIn == "") {
+        alert("서비스 페이지는 로그인 후 사용하실 수 있습니다.");
+        location.href = "/Zoo/login/login.html";
+    } else {
+        location.href = ".html"
+    }
+}
+
 const drawStar = (target) => {
     document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
 }
