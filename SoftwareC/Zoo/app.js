a5f8d82dec24f5a559333b98c9d04c5b488137a5
choi-jimin <109493189+choi-jimin@users.noreply.github.com>
2022-11-18 22:13:13 +0900

    software Update

--- SoftwareC/Zoo/app.js
+++ SoftwareC/Zoo/app.js
@@ -1,63 +1,52 @@
-/* 영화 검색 날짜 */
-let dateObj = new Date();
-let movieSearch;
-if (dateObj.getDate() < 10) {
-    movieSearch = String(dateObj.getFullYear()) + String(dateObj.getMonth() + 1) + "0" + String(dateObj.getDate()) - 1;
-} else {
-    movieSearch = String(dateObj.getFullYear()) + String(dateObj.getMonth() + 1) + String(dateObj.getDate() - 1);
-}
-
-
-/* api 선언부 ---------------------------------------------------------- */
-
-/* 영화진흥위원회 API */
-
-// url : http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json
-
-let api_key_movie = "key=22f10dd3863febae7e67b8df14051d8b"; // API 키
-let targetDt = "&targetDt=" + movieSearch;
-
-const movie_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?" + api_key_movie + targetDt;
-const movie_detail_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?" + api_key_movie + "&movieCd=";
-
-/** 영화진흥위원회 API fetch 함수 */
+/** 일별 박스오피스 호출 API fetch 함수*/
 function callFetchMovie() {
-    fetch(movie_url)
+    fetch("api/search/dailyBoxOffice")
         .then(response => response.json())
         .then(function (json) {
-            console.log(`${movieSearch} 일자의 데이터입니다.`);
-            let movieJson = json.boxOfficeResult.dailyBoxOfficeList;
-            console.log(movieJson)
-            for (let i = 0; i < movieJson.length; i++) {
+            for (let i = 0; i < json.length; i++) {
+                let movieJson = json[i].movies;
+                console.log(movieJson);
+
                 let plusSection = document.getElementById('section_main_div');
 
-                let new_pTag = document.createElement('p');
-                new_pTag.setAttribute('id', 'new_pTag');
+                let new_divTag = document.createElement('div');
+                new_divTag.setAttribute('class', 'new_divTag');
+
+                let new_aTag = document.createElement('a');
+                new_aTag.setAttribute('class', 'new_aTag');
+                new_aTag.setAttribute('href', `/movie/${movieJson.movieCd}`);
 
                 let new_pTag_movieRank = document.createElement('p');
                 let new_pTag_movieImg = document.createElement('img');
-                let new_pTag_movieCd = document.createElement('p');
                 let new_pTag_movieNm = document.createElement('p');
-                let new_pTag_movieOpenDt = document.createElement('p');
+                let new_movieNation_movieOpen = document.createElement('div');
+                let new_pTag_movieOpenDt = document.createElement('span');
+                let new_pTag_movieNation = document.createElement('span');
 
-                new_pTag_movieRank.append(`${i + 1}`);
-                new_pTag_movieImg.append(`이미지`);
-                new_pTag_movieCd.append(`${movieJson[i].movieCd}`);
-                new_pTag_movieNm.append(`${movieJson[i].movieNm}`);
-                new_pTag_movieOpenDt.append(`${movieJson[i].openDt.substr(0, 4)}`);
+                new_pTag_movieRank.setAttribute('class', 'movie_rank');
+                new_pTag_movieNm.setAttribute('class', 'movie_title');
+                new_pTag_movieImg.setAttribute('class', 'movie_img');
+                new_pTag_movieImg.setAttribute('type', 'image');
+                new_pTag_movieImg.setAttribute('src', `${movieJson.poster}`);
+                new_movieNation_movieOpen.setAttribute('class', 'movieNation_movieOpen');
 
-                new_pTag.append(new_pTag_movieRank);
-                new_pTag.append(new_pTag_movieImg);
-                new_pTag.append(new_pTag_movieNm);
-                new_pTag.append(new_pTag_movieCd);
-                new_pTag.append(new_pTag_movieOpenDt);
+                new_pTag_movieRank.append(`${cnt}`);
+                new_pTag_movieNm.append(`${movieJson.movieNm}`);
+                new_pTag_movieOpenDt.append(`${movieJson.openDt.substr(0, 4)}, `);
+                new_pTag_movieNation.append(`${movieJson.nationAlt}`);
+                new_movieNation_movieOpen.append(new_pTag_movieOpenDt);
+                new_movieNation_movieOpen.append(new_pTag_movieNation);
 
-                new_pTag_movieRank.setAttribute('id', 'movie_rank');
-                new_pTag_movieCd.setAttribute('id', 'movie_code');
-                new_pTag_movieNm.setAttribute('id', 'movie_title');
+                new_divTag.append(new_pTag_movieRank);
+                new_divTag.append(new_pTag_movieImg);
+                new_divTag.append(new_pTag_movieNm);
+                new_divTag.append(new_movieNation_movieOpen);
+                new_aTag.append(new_divTag);
 
-                plusSection.appendChild(new_pTag);
-            }
+                plusSection.appendChild(new_aTag);
 
-        });
-}
+                console.log(movieJson.movieCd);
+            } // for end
+
+        }); // second then func end
+} // func end
--- SoftwareC/movie/movie.js
+++ SoftwareC/movie/movie.js
@@ -4,10 +4,76 @@ function service() {
         alert("서비스 페이지는 로그인 후 사용하실 수 있습니다.");
         location.href = "/Zoo/login/login.html";
     } else {
-        location.href = ".html"
+        
     }
 }
 
-const drawStar = (target) => {
-    document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
+/*const drawStar = (target) => {
+    //document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
+    //document.querySelector(`.star span`).style.width = result;
+    document.querySelector(`.star span`).style.width = `80%`;
+    console.log(target.value);
 }
+
+const coloring = () => {
+
+}*/
+
+let star_cnt;
+
+window.onload = function loadStar() {
+    let star = document.querySelectorAll('input');
+    let c_value = document.querySelector('#current_value');
+    let current_value = 0;
+
+    c_value.textContent =  current_value + ' out of 5'
+    
+    for (let i = 0; i < star.length; i++) {
+	    star[i].addEventListener('click', function() {
+		    current_value = this.value;
+
+            c_value.textContent = current_value / 2 + " out of 5";
+    	});
+
+        if(star[i].getAttribute('value') == 3) {
+            star[i].setAttribute('checked', true);
+            c_value.textContent = 3/2 + ' out of 5';
+        }
+    }
+
+    let button_check = false;
+
+    const myButton = document.querySelector('.fa-folder-closed');
+    myButton.addEventListener('click', () => {
+        if(button_check == false) {
+            button_check = true;
+            myButton.classList.replace('fa-folder-closed', 'fa-check');
+        } else {
+            button_check = false;
+            myButton.classList.replace('fa-check', 'fa-folder-closed');
+        }
+    });
+
+    let button_check_w = false;
+
+    const myButton_w = document.querySelector('.fa-laptop');
+    myButton_w.addEventListener('click', () => {
+        if(button_check_w == false) {
+            button_check_w = true;
+            myButton_w.classList.replace('fa-laptop', 'fa-check');
+        } else {
+            button_check_w = false;
+            myButton_w.classList.replace('fa-check', 'fa-laptop');
+        }
+    });
+}
+
+function clickStar(target){
+    let star = target.value;
+
+    star_cnt = star;
+}
+
+
+
+
